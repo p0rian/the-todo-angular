949f4c52019f96bed9d4788aec1012b0713c9c2b
p0rian <porianowski123@gmail.com>
2023-12-27 08:11:12 +0100

    i forgor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { AppComponent } from './app.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CdkDrag } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [AppComponent, ToDoListComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CdkDrag,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
